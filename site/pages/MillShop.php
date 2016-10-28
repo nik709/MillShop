961b4daf7d910e97ba60098f31576fb712ac36cf
Nikita Grechuhin <NikitaGrechukhin@gmail.com>
2016-10-28 21:59:25 +0300

    add test connection

--- site/pages/MillShop.php
+++ site/pages/MillShop.php
@@ -18,7 +18,26 @@ if ($conn){
     echo 'connection is correct';
 }
  -->
-
+<?php
+error_reporting(E_ALL & ~E_DEPRECATED);
+$link = mysql_connect('localhost:3306/test', 'root', 'root');
+if (!$link) {
+    die('Ошибка соединения: ' . mysql_error());
+}
+echo 'Соединение успешно установлено';
+mysql_select_db('test') or die('Не удалось выбрать базу данных');
+$query = 'SELECT * FROM TIME';
+$result = mysql_query($query) or die('Запрос не удался: ' . mysql_error());
+echo "<table>\n";
+while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+    echo "\t<tr>\n";
+    foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+echo "</table>\n";
+?>
 <div id="menu-block">
     <?php
     include('menu.html');
