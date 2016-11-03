52597ef5e710657912216b498acf091bc77392fe
Komarov Nikolay <komarov.nikolay.russia@gmail.com>
2016-11-04 00:22:37 +0300

    Design of the main page

--- site/pages/MillShop.php
+++ site/pages/MillShop.php
@@ -13,30 +13,23 @@
 <body>
 
 <div id="wrapping-block">
-<div id="menu-block">
-    <?php
-    include('menu.html');
-    ?>
-</div>
-
-<div id="main-block">
-    <div class="margin-wrapper">
-    <h1>Welcome to Mill Shop!</h1>
-    <form>
-        <button id="clickme">Click Me!</button>
-    </form>
-
-    <?php
-    include_once("../database/DBConnection.php");
-    $db = new DBConnection();
-    $db->openConnection();
-    $db->selectItemsByColor(0);
-    $db->showResult();
-    $db->closeConnection();
-    ?>
-
+    <div id="menu-block">
+        <?php
+        include('menu.html');
+        ?>
     </div>
-</div>
+
+    <div id="main-block">
+        <div class="margin-wrapper">
+            <div id="banners-block">
+                <div id="main-banner"><img src="../resources/images/banners/banner_40001.jpg"></div>
+                <div id="shop-banners-block">
+                    <div id="banner-shop-men"><a href="Men.php"><img src="../resources/images/banners/banner_40002.jpg"></a></div>
+                    <div id="banner-shop-women"><a href="Women.php"><img src="../resources/images/banners/banner_40003.jpg"></a></div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <div id="footer-block">
