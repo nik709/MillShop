b358a54a4c650233a163bb6cdd76519dd374e528
Komarov Nikolay <komarov.nikolay.russia@gmail.com>
2016-11-04 19:56:21 +0300

    Added sorting

--- site/pages/Men.php
+++ site/pages/Men.php
@@ -15,21 +15,46 @@
     include('menu.html');
     ?>
 
+    <!-- MAIN BLOCK START -->
+
     <h1>Clothes for MEN</h1>
-    <form>
+
+    <!-- SORTING -->
+
+    <form name="sortingForm" method="get">
+        <select name="sortOption" id="sortOption" class="simple-select" onchange="sortingForm.submit()">
+            <option value="" selected disabled style="display:none;">Sort By</option>
+            <option value="ASC">Price: Low to High</option>
+            <option value="DESC">Price: High to Low</option>
+        </select>
         <input type="text" class="simple-textbox" value="">
         <button class="simple-button">Men</button>
     </form>
+    <script type="text/javascript">
+        document.getElementById('sortOption').value = "<?php echo $_GET['sortOption'];?>";
+    </script>
+
+    <?php
+        $sortOption = isset($_GET['sortOption']) ? $_GET['sortOption'] : false;
+        if ($sortOption) {
+            echo htmlentities($_GET['sortOption'], ENT_QUOTES, "UTF-8");
+        } else {
+            echo "option is required";
+        }
+    ?>
 
     <?php
     include_once("../database/DBConnection.php");
     $db = new DBConnection();
     $db->openConnection();
+    $db->sortByPrice($sortOption);
     $criteria[0] = "price < 500";
     $db->selectByCriteria($criteria);
     $db->showResult();
     $db->closeConnection();
     ?>
+
+    <!-- MAIN BLOCK END -->
 
     <?php
     include('footer.html');
