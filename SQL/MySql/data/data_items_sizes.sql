a57cdecb76170b2e35f5ff53f0c5547cabe19779
Nikita Grechuhin <NikitaGrechukhin@gmail.com>
2016-11-15 19:53:55 +0300

    add table of references items and sizes

---
+++ SQL/MySql/data/data_items_sizes.sql
@@ -0,0 +1,165 @@
+insert into `millshop`.`items_sizes` values (1000001, 101);
+insert into `millshop`.`items_sizes` values (1000001, 102);
+insert into `millshop`.`items_sizes` values (1000001, 103);
+insert into `millshop`.`items_sizes` values (1000001, 104);
+insert into `millshop`.`items_sizes` values (1000001, 105);
+insert into `millshop`.`items_sizes` values (1000001, 106);
+
+insert into `millshop`.`items_sizes` values (1000002, 101);
+insert into `millshop`.`items_sizes` values (1000002, 102);
+insert into `millshop`.`items_sizes` values (1000002, 103);
+insert into `millshop`.`items_sizes` values (1000002, 104);
+insert into `millshop`.`items_sizes` values (1000002, 105);
+insert into `millshop`.`items_sizes` values (1000002, 106);
+
+insert into `millshop`.`items_sizes` values (1000003, 101);
+insert into `millshop`.`items_sizes` values (1000003, 102);
+insert into `millshop`.`items_sizes` values (1000003, 103);
+insert into `millshop`.`items_sizes` values (1000003, 104);
+insert into `millshop`.`items_sizes` values (1000003, 105);
+insert into `millshop`.`items_sizes` values (1000003, 106);
+
+insert into `millshop`.`items_sizes` values (1000004, 101);
+insert into `millshop`.`items_sizes` values (1000004, 102);
+insert into `millshop`.`items_sizes` values (1000004, 103);
+insert into `millshop`.`items_sizes` values (1000004, 104);
+insert into `millshop`.`items_sizes` values (1000004, 105);
+insert into `millshop`.`items_sizes` values (1000004, 106);
+
+insert into `millshop`.`items_sizes` values (1000005, 101);
+insert into `millshop`.`items_sizes` values (1000005, 102);
+insert into `millshop`.`items_sizes` values (1000005, 103);
+insert into `millshop`.`items_sizes` values (1000005, 104);
+insert into `millshop`.`items_sizes` values (1000005, 105);
+insert into `millshop`.`items_sizes` values (1000005, 106);
+
+insert into `millshop`.`items_sizes` values (1000006, 101);
+insert into `millshop`.`items_sizes` values (1000006, 102);
+insert into `millshop`.`items_sizes` values (1000006, 103);
+insert into `millshop`.`items_sizes` values (1000006, 104);
+insert into `millshop`.`items_sizes` values (1000006, 105);
+insert into `millshop`.`items_sizes` values (1000006, 106);
+
+insert into `millshop`.`items_sizes` values (1000007, 101);
+insert into `millshop`.`items_sizes` values (1000007, 102);
+insert into `millshop`.`items_sizes` values (1000007, 103);
+insert into `millshop`.`items_sizes` values (1000007, 104);
+insert into `millshop`.`items_sizes` values (1000007, 105);
+insert into `millshop`.`items_sizes` values (1000007, 106);
+
+insert into `millshop`.`items_sizes` values (1000008, 101);
+insert into `millshop`.`items_sizes` values (1000008, 102);
+insert into `millshop`.`items_sizes` values (1000008, 103);
+insert into `millshop`.`items_sizes` values (1000008, 104);
+insert into `millshop`.`items_sizes` values (1000008, 105);
+insert into `millshop`.`items_sizes` values (1000008, 106);
+
+insert into `millshop`.`items_sizes` values (1000009, 101);
+insert into `millshop`.`items_sizes` values (1000009, 102);
+insert into `millshop`.`items_sizes` values (1000009, 103);
+insert into `millshop`.`items_sizes` values (1000009, 104);
+insert into `millshop`.`items_sizes` values (1000009, 105);
+insert into `millshop`.`items_sizes` values (1000009, 106);
+
+insert into `millshop`.`items_sizes` values (1000010, 101);
+insert into `millshop`.`items_sizes` values (1000010, 102);
+insert into `millshop`.`items_sizes` values (1000010, 103);
+insert into `millshop`.`items_sizes` values (1000010, 104);
+insert into `millshop`.`items_sizes` values (1000010, 105);
+insert into `millshop`.`items_sizes` values (1000010, 106);
+
+insert into `millshop`.`items_sizes` values (1000011, 101);
+insert into `millshop`.`items_sizes` values (1000011, 102);
+insert into `millshop`.`items_sizes` values (1000011, 103);
+insert into `millshop`.`items_sizes` values (1000011, 104);
+insert into `millshop`.`items_sizes` values (1000011, 105);
+insert into `millshop`.`items_sizes` values (1000011, 106);
+insert into `millshop`.`items_sizes` values (1000011, 107);
+
+insert into `millshop`.`items_sizes` values (1000012, 101);
+insert into `millshop`.`items_sizes` values (1000012, 102);
+insert into `millshop`.`items_sizes` values (1000012, 103);
+insert into `millshop`.`items_sizes` values (1000012, 104);
+insert into `millshop`.`items_sizes` values (1000012, 105);
+insert into `millshop`.`items_sizes` values (1000012, 106);
+insert into `millshop`.`items_sizes` values (1000012, 107);
+
+insert into `millshop`.`items_sizes` values (1000013, 101);
+insert into `millshop`.`items_sizes` values (1000013, 102);
+insert into `millshop`.`items_sizes` values (1000013, 103);
+insert into `millshop`.`items_sizes` values (1000013, 104);
+insert into `millshop`.`items_sizes` values (1000013, 105);
+insert into `millshop`.`items_sizes` values (1000013, 106);
+insert into `millshop`.`items_sizes` values (1000013, 107);
+
+insert into `millshop`.`items_sizes` values (1000014, 101);
+insert into `millshop`.`items_sizes` values (1000014, 102);
+insert into `millshop`.`items_sizes` values (1000014, 103);
+insert into `millshop`.`items_sizes` values (1000014, 104);
+insert into `millshop`.`items_sizes` values (1000014, 105);
+insert into `millshop`.`items_sizes` values (1000014, 106);
+insert into `millshop`.`items_sizes` values (1000014, 107);
+
+insert into `millshop`.`items_sizes` values (1000015, 101);
+insert into `millshop`.`items_sizes` values (1000015, 102);
+insert into `millshop`.`items_sizes` values (1000015, 103);
+insert into `millshop`.`items_sizes` values (1000015, 104);
+insert into `millshop`.`items_sizes` values (1000015, 105);
+insert into `millshop`.`items_sizes` values (1000015, 106);
+insert into `millshop`.`items_sizes` values (1000015, 107);
+
+insert into `millshop`.`items_sizes` values (1000016, 101);
+insert into `millshop`.`items_sizes` values (1000016, 102);
+insert into `millshop`.`items_sizes` values (1000016, 103);
+insert into `millshop`.`items_sizes` values (1000016, 104);
+insert into `millshop`.`items_sizes` values (1000016, 105);
+insert into `millshop`.`items_sizes` values (1000016, 106);
+insert into `millshop`.`items_sizes` values (1000016, 107);
+
+insert into `millshop`.`items_sizes` values (1000017, 101);
+insert into `millshop`.`items_sizes` values (1000017, 102);
+insert into `millshop`.`items_sizes` values (1000017, 103);
+insert into `millshop`.`items_sizes` values (1000017, 104);
+insert into `millshop`.`items_sizes` values (1000017, 105);
+insert into `millshop`.`items_sizes` values (1000017, 106);
+insert into `millshop`.`items_sizes` values (1000017, 107);
+
+insert into `millshop`.`items_sizes` values (1000018, 101);
+insert into `millshop`.`items_sizes` values (1000018, 102);
+insert into `millshop`.`items_sizes` values (1000018, 103);
+insert into `millshop`.`items_sizes` values (1000018, 104);
+insert into `millshop`.`items_sizes` values (1000018, 105);
+insert into `millshop`.`items_sizes` values (1000018, 106);
+insert into `millshop`.`items_sizes` values (1000018, 107);
+
+insert into `millshop`.`items_sizes` values (1000019, 101);
+insert into `millshop`.`items_sizes` values (1000019, 102);
+insert into `millshop`.`items_sizes` values (1000019, 103);
+insert into `millshop`.`items_sizes` values (1000019, 104);
+insert into `millshop`.`items_sizes` values (1000019, 105);
+insert into `millshop`.`items_sizes` values (1000019, 106);
+insert into `millshop`.`items_sizes` values (1000019, 107);
+
+insert into `millshop`.`items_sizes` values (1000020, 101);
+insert into `millshop`.`items_sizes` values (1000020, 102);
+insert into `millshop`.`items_sizes` values (1000020, 103);
+insert into `millshop`.`items_sizes` values (1000020, 104);
+insert into `millshop`.`items_sizes` values (1000020, 105);
+insert into `millshop`.`items_sizes` values (1000020, 106);
+insert into `millshop`.`items_sizes` values (1000020, 107);
+
+insert into `millshop`.`items_sizes` values (1000025, 101);
+insert into `millshop`.`items_sizes` values (1000025, 102);
+insert into `millshop`.`items_sizes` values (1000025, 103);
+insert into `millshop`.`items_sizes` values (1000025, 104);
+insert into `millshop`.`items_sizes` values (1000025, 105);
+insert into `millshop`.`items_sizes` values (1000025, 106);
+insert into `millshop`.`items_sizes` values (1000025, 107);
+
+insert into `millshop`.`items_sizes` values (1000201, 101);
+insert into `millshop`.`items_sizes` values (1000201, 102);
+insert into `millshop`.`items_sizes` values (1000201, 103);
+insert into `millshop`.`items_sizes` values (1000201, 104);
+insert into `millshop`.`items_sizes` values (1000201, 105);
+insert into `millshop`.`items_sizes` values (1000201, 106);
+insert into `millshop`.`items_sizes` values (1000201, 107);
