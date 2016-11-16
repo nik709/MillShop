a9ba181877283b6ee6ef0f0472943e2728b3dd6c
Komarov Nikolay <komarov.nikolay.russia@gmail.com>
2016-11-16 23:34:12 +0300

    Created price slider

---
+++ site/pages/scripts/getMaxPriceForSlider.php
@@ -0,0 +1,12 @@
+<?php
+/**
+    MILL SHOP COMPANY, 2016
+    CREATED BY NIKITA GRECHUKHIN, NIKOLAY KOMAROV AND VAGIK SIMONYAN
+ */
+
+require_once "../../database/QueryPresenter.php";
+//$dataObj = new QueryPresenterImpl();
+//echo json_decode($dataObj->getMaxPrice());
+//echo json_decode($dataObj->getMaxPrice());
+echo "196";
+?>
