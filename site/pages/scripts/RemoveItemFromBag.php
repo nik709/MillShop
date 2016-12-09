e3b60573f2a582e16dc09fcace7377d634cd0a57
Komarov Nikolay <komarov.nikolay.russia@gmail.com>
2016-12-10 01:29:43 +0300

    Removed comments

--- site/pages/scripts/RemoveItemFromBag.php
+++ site/pages/scripts/RemoveItemFromBag.php
@@ -4,12 +4,10 @@
     CREATED BY NIKITA GRECHUKHIN, NIKOLAY KOMAROV AND VAGIK SIMONYAN
  */
 session_start();
+
 $id = $_GET['valueId'];
 $_SESSION['count'] -= $_SESSION['quant'][$id];
 unset($_SESSION['item'][$id]);
 unset($_SESSION['size'][$id]);
 unset($_SESSION['quant'][$id]);
-/*$_SESSION['item'] = array_diff($_SESSION['item'], $_SESSION['item'][$id]);
-$_SESSION['size'] = array_diff($_SESSION['size'], $_SESSION['size'][$id]);
-$_SESSION['quant'] = array_diff($_SESSION['quant'], $_SESSION['quant'][$id]);*/
 ?>
